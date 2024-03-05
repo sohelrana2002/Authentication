5f82ac969561535a363c0a63c0e4aa067a523c3d
SOHEL RANA <sohelrana104322@gmail.com>
2024-03-05 12:32:05 +0600

    add: password validation

--- clients/src/pages/LogIn/LogIn.jsx
+++ clients/src/pages/LogIn/LogIn.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
-import './LogIn.css'
+import React from "react";
+import { NavLink} from "react-router-dom";
+import "./LogIn.css";
 
 const LogIn = () => {
   return (
-    <div>LogIn</div>
-  )
-}
+    <div className="signup">
+      <form>
+        <h3 className="title">Log In</h3>
+        <div className="form__content">
+          <input className="user__input" type="text" placeholder="Email" />
+          <input
+            className="user__input"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
 
-export default LogIn
+        <NavLink className="forget">
+          <p>Forget password?</p>
+        </NavLink>
+        <button type="submit" className="btn">
+          Log In
+        </button>
+
+        <p className="text">
+          Don't have an account? <NavLink to="/sign-up">Create one</NavLink>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default LogIn;
--- clients/src/pages/SignUp/SignUp.jsx
+++ clients/src/pages/SignUp/SignUp.jsx
@@ -1,35 +1,76 @@
-import React from "react";
-import { NavLink} from "react-router-dom"
+import React, { useState } from "react";
+import { NavLink} from "react-router-dom";
 
 import "./SignUp.css";
 
 const SignUp = () => {
+  const [userSignUp, setUserSignUp] = useState({
+    email: "",
+    password: "",
+    cPassword: "",
+    wrongPass: false
+  });
+
+  const handleInput = (e) =>{
+    let name = e.target.name;
+    let value = e.target.value;
+    setUserSignUp({
+      ...userSignUp,
+      [name] : value,
+    });
+  }
+  console.log(userSignUp);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(userSignUp.password === userSignUp.cPassword){
+
+    }else{
+      setUserSignUp({
+        ...userSignUp,
+        wrongPass: true,
+      })
+    }
+  }
+  
   return (
     <div className="signup">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3 className="title">Sign UP</h3>
         <div className="form__content">
           <input
             className="user__input"
             type="text"
             placeholder="Email"
+            name="email"
+            value={userSignUp.email}
+            onChange={handleInput}
           />
           <input
             className="user__input"
             type="password"
             placeholder="Password"
+            name="password"
+            value={userSignUp.password}
+            onChange={handleInput}
           />
           <input
             className="user__input"
             type="password"
             placeholder="Confirm password"
+            name="cPassword"
+            value={userSignUp.cPassword}
+            onChange={handleInput}
           />
         </div>
 
-        <NavLink className="forget">
-          <p>Forget password?</p>
-        </NavLink>
-        <button type="submit" className="btn">Submit</button>
+        {
+          userSignUp.wrongPass && <div className="wrongPass">
+          <p>Password is'n same</p>
+          </div>
+        }
+
+        <button type="submit" className="btn">Sign Up</button>
 
         <p className="text">Have an account? <NavLink to='/'>Log In</NavLink></p>
       </form>
