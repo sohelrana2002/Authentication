038089189d29b227bd9dbfa9d394fa76b6f3f663
SOHEL RANA <sohelrana104322@gmail.com>
2024-03-10 16:44:41 +0600

    add: validator using zod

--- server/src/routers/auth-router.js
+++ server/src/routers/auth-router.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = new express.Router();
 const authController = require("../controller/auth-controller");
+const signUpValidatorSchema = require("../validator/auth-validator");
+const validate = require("../middlewares/validate-middleware");
 
 
 router.route("/signUp-data").get(authController.getSignUpData);
 // =====for sign up=====
-router.route("/sign-up").post(authController.register);
+router
+  .route("/sign-up")
+  .post(validate(signUpValidatorSchema), authController.register);
 
 // =====for log in=====
 router.route("/login").get(authController.login);
-
 
 
 module.exports = router;
---
+++ server/src/validator/auth-validator.js
@@ -0,0 +1,26 @@
+const { z } = require("zod");
+
+// ====creating an object  schema========
+const signUpValidatorSchema = z.object({
+    email: z
+    .string({required_error: "Email must be required"})
+    .trim()
+    .email({ message: "Invalid email "}),
+    password: z
+    .string({required_error: "Password is required"})
+    .min(6, { message: "Password must be at least 6 character"})
+    .max(1024, { message: "Password can't be greater than 1024 character"}),
+    cPassword: z
+    .string({required_error: "Confirm password must be same as password"})
+    .min(6, { message: "Password must be at least 6 character"})
+    .max(1024, { message: "Password can't be greater than 1024 character"})
+}).superRefine(({ password, cPassword }, ctx) => {
+    if (password !== cPassword) {
+      ctx.addIssue({
+        code: "custom",
+        message: "The passwords did not match"
+      });
+    }
+})
+
+module.exports = signUpValidatorSchema
