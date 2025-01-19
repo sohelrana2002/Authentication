51e291b45275e8d880ef82171e16d7fb68dcf584
SOHEL RANA <sohelrana104322@gmail.com>
2025-01-20 02:04:48 +0600

    fixed issues in validate-middleware.js file

--- server/src/middlewares/validate-middleware.js
+++ server/src/middlewares/validate-middleware.js
@@ -1,13 +1,15 @@
-const validate = (schema) => async(req, res, next) =>{
-    try{
-        const parseBody = await schema.parseAsync(req.body);
-        req.body = parseBody;
-        next();
-    }catch(err){
-        res.status(400).json({
-            message: err.issues[0,1].message
-        })
-    }
+const validate = (schema) => async (req, res, next) => {
+  try {
+    const parseBody = await schema.parseAsync(req.body);
+    req.body = parseBody;
+    next();
+  } catch (err) {
+    // console.log(err, "err");
+
+    res.status(400).json({
+      message: err?.issues[0]?.message,
+    });
+  }
 };
 
 module.exports = validate;
